06e732439f2560292d3fe108a817326a449a87d5
Nishka Maharaj <Nishka.Maharaj@chillisoft.co.za>
2023-06-23 16:06:51 +0200

    initial commit

---
+++ XamarinReview/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using XamarinReview.ViewModels;
+using XamarinReview.Views;
+
+namespace XamarinReview
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
---
+++ XamarinReview/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using XamarinReview.ViewModels;
+
+namespace XamarinReview.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
